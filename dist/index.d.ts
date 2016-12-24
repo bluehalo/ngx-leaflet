5add56f08a25123d30d0fcc1f7fd0d7f02ce9d61
reblace <reblace@gmail.com>
2016-12-23 19:36:35 -0500

    Exposing the directive wrapper class

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
 export { LeafletModule } from './leaflet/leaflet.module';
 export { LeafletDirective } from './leaflet/core/leaflet.directive';
+export { LeafletDirectiveWrapper } from './leaflet/core/leaflet.directive.wrapper';
--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,7 @@ var leaflet_module_1 = require('./leaflet/leaflet.module');
 exports.LeafletModule = leaflet_module_1.LeafletModule;
 var leaflet_directive_1 = require('./leaflet/core/leaflet.directive');
 exports.LeafletDirective = leaflet_directive_1.LeafletDirective;
+var leaflet_directive_wrapper_1 = require('./leaflet/core/leaflet.directive.wrapper');
+exports.LeafletDirectiveWrapper = leaflet_directive_wrapper_1.LeafletDirectiveWrapper;
 
 //# sourceMappingURL=index.js.map
