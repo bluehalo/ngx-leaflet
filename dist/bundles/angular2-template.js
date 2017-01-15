72a791826a43d3a60beb82470af44902bc658703
reblace <reblace@gmail.com>
2017-01-15 17:52:27 -0500

    Updating packages

--- dist/bundles/angular2-template.js
+++ dist/bundles/angular2-template.js
@@ -5,8 +5,19 @@
 	(factory());
 }(this, (function () { 'use strict';
 
-var example_module_1 = require('./example/example.module');
-exports.ExampleModule = example_module_1.ExampleModule;
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "./example/example.module"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    var example_module_1 = require("./example/example.module");
+    exports.ExampleModule = example_module_1.ExampleModule;
+});
 
 })));
 //# sourceMappingURL=angular2-template.js.map
