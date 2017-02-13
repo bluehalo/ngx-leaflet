0d7091f2bb9945c78d341d9c4f590e4871af7bad
Ryan Blace <reblace@gmail.com>
2017-02-13 16:38:53 -0500

    Updating to remove dependency on ts-helpers

--- dist/bundles/angular2-template.js
+++ dist/bundles/angular2-template.js
@@ -1,10 +1,16 @@
-/*! @asymmetrik/angular2-template-0.2.0 - Copyright Asymmetrik, Ltd. 2007-2017 - All Rights Reserved.*/
+/*! @asymmetrik/angular2-template-0.2.1 - Copyright Asymmetrik, Ltd. 2007-2017 - All Rights Reserved.*/
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core')) :
 	typeof define === 'function' && define.amd ? define(['exports', '@angular/core'], factory) :
 	(factory((global.angular2Template = global.angular2Template || {}),global.ng.core));
 }(this, (function (exports,_angular_core) { 'use strict';
 
+var __decorate$1 = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 var ExampleComponent = (function () {
     function ExampleComponent() {
         this.message = 'Hello';
@@ -17,13 +23,19 @@ var ExampleComponent = (function () {
     };
     return ExampleComponent;
 }());
-ExampleComponent = __decorate([
+ExampleComponent = __decorate$1([
     _angular_core.Component({
         selector: 'example-component',
         template: '{{message}}'
     })
 ], ExampleComponent);
 
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 exports.ExampleModule = (function () {
     function ExampleModule() {
     }
