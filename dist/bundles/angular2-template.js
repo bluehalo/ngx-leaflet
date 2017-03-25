82faff1d1e4e826cef3cd57303c3b0355141bd9d
Ryan Blace <reblace@gmail.com>
2017-03-25 18:58:20 -0400

    Fixing aot issue

--- dist/bundles/angular2-template.js
+++ dist/bundles/angular2-template.js
@@ -1,16 +1,10 @@
-/*! @asymmetrik/angular2-template-1.0.0 - Copyright Asymmetrik, Ltd. 2007-2017 - All Rights Reserved.*/
+/*! @asymmetrik/angular2-template-1.1.0 - Copyright Asymmetrik, Ltd. 2007-2017 - All Rights Reserved.*/
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core')) :
 	typeof define === 'function' && define.amd ? define(['exports', '@angular/core'], factory) :
 	(factory((global.angular2Template = global.angular2Template || {}),global.ng.core));
 }(this, (function (exports,_angular_core) { 'use strict';
 
-var __decorate$1 = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
 var ExampleComponent = (function () {
     function ExampleComponent() {
         this.message = 'Hello';
@@ -23,34 +17,33 @@ var ExampleComponent = (function () {
     };
     return ExampleComponent;
 }());
-ExampleComponent = __decorate$1([
-    _angular_core.Component({
-        selector: 'example-component',
-        template: '{{message}}'
-    })
-], ExampleComponent);
+ExampleComponent.decorators = [
+    { type: _angular_core.Component, args: [{
+                selector: 'example-component',
+                template: '{{message}}'
+            },] },
+];
+/** @nocollapse */
+ExampleComponent.ctorParameters = function () { return []; };
 
-var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-exports.ExampleModule = ExampleModule_1 = (function () {
+var ExampleModule = (function () {
     function ExampleModule() {
     }
     ExampleModule.forRoot = function () {
-        return { ngModule: ExampleModule_1, providers: [] };
+        return { ngModule: ExampleModule, providers: [] };
     };
     return ExampleModule;
 }());
-exports.ExampleModule = ExampleModule_1 = __decorate([
-    _angular_core.NgModule({
-        exports: [ExampleComponent],
-        declarations: [ExampleComponent]
-    })
-], exports.ExampleModule);
-var ExampleModule_1;
+ExampleModule.decorators = [
+    { type: _angular_core.NgModule, args: [{
+                exports: [ExampleComponent],
+                declarations: [ExampleComponent]
+            },] },
+];
+/** @nocollapse */
+ExampleModule.ctorParameters = function () { return []; };
+
+exports.ExampleModule = ExampleModule;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
