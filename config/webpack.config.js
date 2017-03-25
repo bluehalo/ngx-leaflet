409b89e90b81dce9d77df2c1654efe5c2def02c8
Ryan Blace <reblace@gmail.com>
2017-03-25 11:27:14 -0600

    Upgrade to ng2 v4

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -120,7 +120,8 @@ module.exports = () => {
 			minChunks: Infinity
 		}),
 		new webpack.ContextReplacementPlugin(
-			/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/
+			/angular(\\|\/)core(\\|\/)@angular/,
+			path.posix.resolve('./src')
 		)
 	);
 
