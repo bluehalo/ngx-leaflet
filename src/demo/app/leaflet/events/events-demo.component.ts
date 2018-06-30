a923ec4caddb9582364c163dd363c9cc8885849e
Ryan Blace <reblace@gmail.com>
2018-06-30 13:32:09 -0400

    Cleaned up demo code and removed yarn lock from gitignore

--- src/demo/app/leaflet/events/events-demo.component.ts
+++ src/demo/app/leaflet/events/events-demo.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { debounceTime, scan } from 'rxjs/operators';
 
-import { latLng, LatLng, tileLayer } from 'leaflet';
+import { latLng, tileLayer } from 'leaflet';
 
 
 @Component({
@@ -11,23 +11,20 @@ import { latLng, LatLng, tileLayer } from 'leaflet';
 })
 export class LeafletEventsDemoComponent {
 
-	zoom: number = 5;
-
-	center: LatLng = latLng([ 46.879966, -121.726909 ]);
+	eventCount = 0;
 	eventLog: string = '';
 
 	options = {
-		zoom: this.zoom,
-		center: this.center
+		zoom: 5,
+		center: latLng([ 46.879966, -121.726909 ])
 	};
-
 	baselayer = tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' });
 
 	eventSubject = new Subject<string>();
 
 	constructor() {
 		this.eventSubject.pipe(
-				scan((acc: string, v: string) => `${v}\n${acc}`, ''),
+				scan((acc: string, v: string) => `${++this.eventCount}: ${v}\n${acc}`, ''),
 				debounceTime(50)
 			)
 			.subscribe((v) => { this.eventLog = v; } );
