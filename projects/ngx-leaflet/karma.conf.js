b6814abcd1acfdda4134743178d3555350a79a0a
Ryan Blace <reblace@gmail.com>
2022-02-20 21:54:21 -0500

    cleanup and removing the empty readme from the library

--- projects/ngx-leaflet/karma.conf.js
+++ projects/ngx-leaflet/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-sentio'),
+      dir: require('path').join(__dirname, '../../coverage/ngx-leaflet'),
       subdir: '.',
       reporters: [
         { type: 'html' },
