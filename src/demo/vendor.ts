0df891c68c927446af811dd4aa5c8dfdadc602b2
Ryan Blace <reblace@gmail.com>
2022-02-20 20:03:35 -0500

    removing unused demo dir and making a few small fixes to the demo

--- src/demo/vendor.ts
+++
@@ -1,4 +0,0 @@
-// This addresses a weird thing with how Leaflet handles icon URLs. See README for details.
-// import 'leaflet/dist/images/marker-shadow.png';
-// import 'leaflet/dist/images/marker-icon.png';
-// import 'leaflet/dist/images/marker-icon-2x.png';
