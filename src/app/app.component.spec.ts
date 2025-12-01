e4763a7147c0b185e691ded2790a0c9f1437fdff
Andreas Nicolaou <anicolaou66@gmail.com>
2025-12-01 17:14:36 +0200

    refactor: update component templates to use self-closing tags and improve button styling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,8 +4,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [AppComponent],
-        }).compileComponents();
+    imports: [AppComponent],
+}).compileComponents();
     });
 
     it('should create the app', () => {
